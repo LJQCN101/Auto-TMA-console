9d66bdcaebeb64486c8dc420431d6fbcbf69c46a
LJQCN101 <someone153@126.com>
2024-03-28 17:13:28 +0800

    Fix python implementation
    Fix a bug of not accounting ownship (m0, n0) coordinate.

--- auto_TMA_python.py
+++ auto_TMA_python.py
@@ -1,45 +1,44 @@
 import numpy as np
 from scipy.optimize import minimize
 
-def bearing_vector(idx, t, crs, spd, L1_distance, own_positions, bearings):
-    brg = np.radians(bearings)
-    u = spd * np.sin(crs)
-    v = spd * np.cos(crs)
-    
-    a = L1_distance * np.sin(brg[0])
-    b = L1_distance * np.cos(brg[0])
-    
-    m, n = own_positions[idx]
-    
-    x = a + u * t
-    y = b + v * t
-    
-    return (y - n) * np.sin(brg[idx]) - (x - m) * np.cos(brg[idx])
 
-
-def sum_of_squares(params, own_positions, bearings, time_intervals):
+def sum_of_squares(params, own_positions, bearings, time_steps):
     L1_distance, crs, spd = params
+    crs_rad = np.radians(crs)
+    brg = np.radians(bearings)
+    
+    # target speed components
+    u = spd * np.sin(crs_rad)
+    v = spd * np.cos(crs_rad)
+    
+    # target position at first bearing
+    m0, n0 = own_positions[0]
+    a = m0 + L1_distance * np.sin(brg[0])
+    b = n0 + L1_distance * np.cos(brg[0])
+    
+    # sum of errors
     f_sum = 0
-    
-    for idx, t in enumerate(time_intervals):
-        f_t = bearing_vector(idx, t, crs, spd, L1_distance, own_positions, bearings)
+    for idx in range(len(time_steps)):
+        m, n = own_positions[idx]
+        x = a + u * time_steps[idx]
+        y = b + v * time_steps[idx]
+        f_t = (y - n) * np.sin(brg[idx]) - (x - m) * np.cos(brg[idx])
         f_sum += f_t ** 2
-    
     return f_sum
 
+
 # Input data
-own_positions = [(0, 0), (0, 0), (0, 0), (300, 300)]  # Example positions at each time interval
-bearings = [0, 10, 20, 20]  # Example bearings in degrees
-time_intervals = [0, 120, 240, 360]  # Example time intervals between bearings
+own_positions = [(262, 505), (255, 505), (248, 505), (240, 505)]  # Example positions at each time interval
+bearings = [153.61687, 154.629, 155.75335, 156.89733]  # Example bearings in degrees
+time_steps = [0, 60, 120, 180]  # Example 60 time intervals between bearings
 
 # Initial guess for L1_distance, crs, and spd
-initial_guess = [5000, 0, 3]
-
-# Optimize the sum of squares using the BFGS algorithm
-res = minimize(sum_of_squares, initial_guess, args=(own_positions, bearings, time_intervals),
-                method='BFGS')
+initial_guess = [5000.0, 180.0, 5.0]
+res = minimize(sum_of_squares, initial_guess, args=(own_positions, bearings, time_steps), method='BFGS')
+error = sum_of_squares(res.x, own_positions, bearings, time_steps)
 
 L1_distance, crs, spd = res.x
-print('Optimized Target Distance at First Bearing:', L1_distance)
-print('Optimized Target Course:', np.degrees(crs))
-print('Optimized Target Speed:', spd * 1.94384449)
+print('Optimized Target Distance at First Bearing: {0} pixel'.format(L1_distance))
+print('Optimized Target Course: {0} deg'.format(crs))
+print('Optimized Target Speed: {0} pixel/s'.format(spd))
+print('Error: {0}, should be less than 1e-6'.format(error))
